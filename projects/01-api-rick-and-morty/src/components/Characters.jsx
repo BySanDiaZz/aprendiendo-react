128823dca28cd202abaa66f1fc9e4e257c1b39d3
sariza <sariza@intelix.biz>
2024-06-10 16:57:28 -0400

    commit 10/06/2024

--- projects/01-api-rick-and-morty/src/components/Characters.jsx
+++ projects/01-api-rick-and-morty/src/components/Characters.jsx
@@ -9,16 +9,44 @@ export const Characters = ({...props}) => {
       <h1>
         Desde Characters JSX
       </h1>
-      <button className="back-home">Volver</button>
       <div className="container-characters">
         {
           characters.map((character, index)=>(
             <div className="character-container" key={index}>
-              <p>{character.name}</p>
+              <div>
+                <img src={character.image} alt={character.name} />
+              </div>
+              <div>
+                <h3>{character.name}</h3>
+                <h6>
+                  {
+                    character.status === 'Alive' ? (
+                      <> 
+                        <span className="alive"/>
+                        Alive
+                      </>
+                    ) : (
+                      <>
+                        <span className="dead"/>
+                        Dead
+                      </>
+                    )
+                  }
+                </h6>
+                <p>
+                  <span className="text-grey">Episodios: </span>
+                  <span>{character.episode.length}</span>
+                </p>
+                <p>
+                  <span className="text-grey">Especie: </span>
+                  <span>{character.species}</span>
+                </p>
+              </div>
             </div>
           ))
         }
       </div>
+      <button className="back-home">Volver al Home</button>
     </div>
   )
 }
